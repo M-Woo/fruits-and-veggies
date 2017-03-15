0e7b71ed42938aa4dd9219a04298171c2f947141
m-woo <mark.chi.woo@gmail.com>
2017-03-15 09:00:10 -0700

    functioning buttons

--- js/app.js
+++ js/app.js
@@ -1,5 +1,53 @@
 /* setup your angular app here */
+console.log('test started')
+var app = angular.module('FruitList', []);
+app.controller('fruitCtrl', ['$scope', function($scope){
+	 $scope.fruitList = [];
+	 $scope.vegiList = [];
+	 $scope.mix = [];
 
+	$scope.left = function(){
+		if(true){
+			$scope.fruitList.push($scope.mix[this.$index]);
+			$scope.mix.splice(this.$index, 1);
+		}
+	}
+	$scope.right = function(){
+		if(true){
+			$scope.vegiList.push($scope.mix[this.$index]);
+			$scope.mix.splice(this.$index, 1);
+		}
+	}
+	$scope.leftCenter = function(){
+		if(true){
+			$scope.mix.push($scope.fruitList[this.$index]);
+			$scope.fruitList.splice(this.$index, 1);
+		}
+	}
+	$scope.rightCenter = function(){
+		if(true){
+			$scope.mix.push($scope.vegiList[this.$index]);
+			$scope.vegiList.splice(this.$index, 1);
+		}
+	}
+
+	$scope.fruits = [
+	  'Apple',
+	  'Apricot',
+	  'Banana'
+	];
+	$scope.vegetables = [
+	  'Artichoke',
+	  'Arugula',
+	  'Asparagus'
+	];
+
+
+
+	 $scope.mix = $scope.fruits.concat($scope.vegetables);
+
+
+}])
 // debug stuff to show the app is loading and fruit / veggies are available
 console.log('App Started');
 console.log('Fruit count', fruits.length);
--- js/fruits_and_veggies_test.js
+++ js/fruits_and_veggies_test.js
@@ -1,14 +1,16 @@
 // this file defines 2 SHORT arrays for you
 // this is good (less annoying) for testing
 
-var fruits = [
-  'Apple',
-  'Apricot',
-  'Banana'
-];
+	var fruits = [
+	  'Apple',
+	  'Apricot',
+	  'Banana'
+	];
 
-var vegetables = [
-  'Artichoke',
-  'Arugula',
-  'Asparagus'
-];
+	var vegetables = [
+	  'Artichoke',
+	  'Arugula',
+	  'Asparagus'
+	];
+
+
